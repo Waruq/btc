db3dec4b2d1cd650319407f3ec7f2edb92386e12
Sheikh Taha <tahalatief@gmail.com>
2017-12-02 22:55:09 +0530

    new commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -288,7 +288,6 @@ svg:nth-of-type(10) ellipse {
 	</style>
 
 
-
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
